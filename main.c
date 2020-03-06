9c1a8c657abbf496e17475130ac71df3c0ee1ff2
lillypuppala77 <lilly.puppala77@gmail.com>
2020-03-06 17:24:01 +0530

    global variables setup

--- main.c
+++ main.c
@@ -1,4 +1,33 @@
 #include <stdio.h>
+struct process_details
+{
+	int process_num;
+	int arrival_time_t;
+	int waiting_time;
+	int turnaround_time;
+	int prior;
+	int burst2;
+	int burst1;
+} Queue_1[10], Queue_2[10], temporary;
+
+
+//Global vars
+int    i; 
+int    n;
+int    x;
+int    j;
+int    k;
+int    b = 0;
+int    time_copy = 0;
+int    time_quantum1;
+int    time_quantum2;
+int    totpro;
+int    countn = 0;
+int    totpro2;
+int    check = 0;
+float  wait_time = 0;
+float  turnt = 0;
+
 int main(){
 
 printf("this is os project");}
