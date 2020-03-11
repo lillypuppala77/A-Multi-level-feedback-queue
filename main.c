e3040ab2b68bc0f69f548972044f34a023032905
lillypuppala77 <lilly.puppala77@gmail.com>
2020-03-11 17:28:52 +0530

    input  setup

--- main.c
+++ main.c
@@ -28,6 +28,34 @@ int    check = 0;
 float  wait_time = 0;
 float  turnt = 0;
 
-int main(){
+void input_and_setup() {
+	printf("\n Enter no. Of processes:\t");
+	scanf("%d", &totpro);
+	n = totpro;
 
+	printf("\nEnter Time Quantum for Fixed priority queue in multiples of 2:-");
+	scanf("%d", &time_quantum1);
+	printf("\nEnter Time Quantum for Round Robin queue in multiples of 2:-");
+	scanf("%d", &time_quantum2);
+	
+	for (i = 0; i < totpro; i++)
+	{
+		printf("\nEnter the Process number:-");
+
+		scanf("%d", &Queue_1[i].process_num);
+
+		printf("Enter  the Arrival Time for the process.....time at which the process arrives in the ready queue:-");
+		scanf("%d", &Queue_1[i].arrival_time_t);
+		printf("Enter the Burst Time for the process.....the amount of time required by a process for executing on CPU.:-");
+		scanf("%d", &Queue_1[i].burst1);
+		Queue_1[i].burst2 = Queue_1[i].burst1;
+		printf("Enter the Priority for the process.....higher priority is carried out first:\t");
+		scanf("%d", &Queue_1[i].prior);
+	}
+
+}
+
+
+int main(){
+input_and_setup();
 printf("this is os project");}
